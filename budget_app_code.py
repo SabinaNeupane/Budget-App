5673f15e53c2576e559b6c19c2a75c8762ed37c5
SabinaNeupane <sabina.neuwork@gmail.com>
2026-01-11 14:30:46 +0545

    added transfer and balance methods

--- budget_app_code.py
+++ budget_app_code.py
@@ -6,3 +6,13 @@ class Category():
         self.ledger.append({'amount':amt,'description':description})
     def withdraw(self,amt,description):
         self.ledger.append({'amount':-amt,'description':description})
+    def get_balance(self):
+        bal=0
+        for idx in self.ledger:
+            bal=bal+idx["amount"]
+        return bal
+    def transfer(self,amt,u2):
+        self.ledger.append({'amount':-amt,'description':f"Transfer to {u2.name}"})
+        u2.ledger.append({'amount':amt,'description':f"Transfer from {self.name}"})
+    def check_funds(self,amt):
+        return amt<=self.get_balance()
