fee1b8126078911421c1f83c215eba343fafa756
SabinaNeupane <sabina.neuwork@gmail.com>
2026-01-11 14:29:47 +0545

    added deposit and withdrawal methods

--- budget_app_code.py
+++ budget_app_code.py
@@ -2,3 +2,7 @@ class Category():
     def __init__(self,name):
         self.name=name
         self.ledger=[]
+    def deposit(self,amt,description):
+        self.ledger.append({'amount':amt,'description':description})
+    def withdraw(self,amt,description):
+        self.ledger.append({'amount':-amt,'description':description})
