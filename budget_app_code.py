6bd34170345149c25484d381ddc9b80088d880b4
SabinaNeupane <sabina.neuwork@gmail.com>
2026-01-11 14:31:23 +0545

    added string format

--- budget_app_code.py
+++ budget_app_code.py
@@ -16,3 +16,12 @@ class Category():
         u2.ledger.append({'amount':amt,'description':f"Transfer from {self.name}"})
     def check_funds(self,amt):
         return amt<=self.get_balance()
+    def __str__(self):
+        tittle= f"{self.name:*^30}\n"
+        line=""
+        for idx in self.ledger:
+            des=idx["description"][0:23]
+            amt=f"{idx["amount"]:.2f}"
+            line+=f"{des:<23}{amt:>7}\n"
+        total=f"Total: {self.get_balance():.2f}\n"
+        return tittle+line+total
